4f3fc8c826c0dfa5ef0a1913c036e48d44e07f59
Kaibo <haokaibo@outlook.com>
2015-01-11 22:16:46 +0800

    1. Renamed InsertionSorting to InsertionSort. 2. Added InsertionSortPart2 and related test case.

--- AmazonTest/src/com/hackerrank/test/arraysorting/InsertionSort.java
+++ AmazonTest/src/com/hackerrank/test/arraysorting/InsertionSort.java
@@ -1,11 +1,33 @@
 package com.hackerrank.test.arraysorting;
 
-public class InsertionSorting {
+public class InsertionSort {
 
 	public static void main(String[] args) {
 
 	}
-
+	public static void insertionSortPart2(int[] ar) {
+		// Fill up the code for the required logic here
+		// Manipulate the array as required
+		// The code for Input/Output is already provided
+		if (ar == null || ar.length == 0)
+			return;
+		if (ar.length == 1) {
+			printArray(ar);
+			return;
+		}
+		int temp;
+		for (int i = 1; i < ar.length; i++) {
+			int j = i;
+			while (j > 0 && ar[j] < ar[j - 1]) {
+				temp = ar[j];
+				ar[j] = ar[j - 1];
+				ar[j - 1] = temp;
+				j--;
+			}
+			printArray(ar);
+		}
+	}
+	
 	public static void insertIntoSorted(int[] ar) {
 		if (ar == null || ar.length == 0)
 			return;
--- AmazonTest/src/com/hackerrank/test/ut/InsertionSortTestCase.java
+++ AmazonTest/src/com/hackerrank/test/ut/InsertionSortTestCase.java
@@ -4,27 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.hackerrank.test.arraysorting.InsertionSorting;
+import com.hackerrank.test.arraysorting.InsertionSort;
 
-public class InsertionSortingTestCase {
+public class InsertionSortTestCase {
 
 	@Test
 	public void testInsertIntoSorted() {
 		int[] arr = { 2, 4, 6, 8, 3 };
-		InsertionSorting.insertIntoSorted(arr);
+		InsertionSort.insertIntoSorted(arr);
 	}
 	
 	@Test
 	public void testInsertIntoSortedWhenArrayIsSorted() {
 		int[] arr = { 1, 2, 3, 4, 5 };
-		InsertionSorting.insertIntoSorted(arr);
+		InsertionSort.insertIntoSorted(arr);
 	}
 	
 	@Test
 	public void testInsertIntoSortedWhenArrayIsNotSorted() {
 		int[] arr = { 2, 3, 1 };
-		InsertionSorting.insertIntoSorted(arr);
+		InsertionSort.insertIntoSorted(arr);
 	}
 	
-	
+	@Test
+	public void testInsertionSortPart2(){
+		int[] arr ={1, 4, 3, 5, 6, 2};
+		InsertionSort.insertionSortPart2(arr);
+	}
 }
